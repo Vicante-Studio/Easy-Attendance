7133d23451daab3b7bb3da326039ebed01cf6ad3
Batubo Victory <victorybatubo76@gmail.com>
2026-05-07 15:57:50 +0100

    feat: Add basic content to homePage

--- app/client/src/pages/HomePage.tsx
+++ app/client/src/pages/HomePage.tsx
@@ -1,21 +1,43 @@
 import { Button } from '@/components/ui/button'
+import { Card, CardHeader, CardTitle, CardDescription, CardContent } from '@/components/ui/card'
+import { useNavigate } from 'react-router'
 
 const HomePage = () => {
+    const navigate = useNavigate()
   return (
-    <div>
-      <h1 className='text-5xl text-center font-bold'>
-        LFC Easy Counter
-      </h1>
+    <main className='text-center p-4 h-screen flex flex-col justify-center gap-16'> 
+      <section className='flex flex-col gap-2'>
+        <h1 className='text-5xl text-center font-bold'>
+            Easy Counter
+        </h1>
 
-      <h2>
-        Choose your job
-      </h2>
+        <h2 className='text-2xl'>
+            Simple live attendance counting for church services.
+        </h2>
+      </section>
 
-      <div>
-        <Button> I'm a Counter </Button>
-        <Button> Admin Dashboard </Button>
-      </div>
-    </div>
+      <section className='flex w-full justify-between gap-2'>
+        <Card className='w-fit py-4 px-2 text-center justify-between'>
+            <CardHeader>
+                <CardTitle>I'm a Counter</CardTitle>
+                <CardDescription>Submit attendance numbers for your assigned section.</CardDescription>
+            </CardHeader>
+            <CardContent>
+                <Button onClick={() => navigate('/counterPage')}> Open Counter Page </Button>
+            </CardContent>
+        </Card>
+
+        <Card className='max-w-sm w-fit p-2 text-center'>
+            <CardHeader>
+                <CardTitle>I'm an Admin</CardTitle>
+                <CardDescription>Manage services, monitor live totals, and export reports.</CardDescription>
+            </CardHeader>
+            <CardContent>
+                <Button onClick={() => navigate('/adminPage')}> Open Admin Dashboard </Button>
+            </CardContent>
+        </Card>
+      </section>
+    </main>
   )
 }
 
