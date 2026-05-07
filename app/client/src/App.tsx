5058d176eca21d7e8b0bb81a78b62d7244f0bcfe
Batubo Victory <victorybatubo76@gmail.com>
2026-05-07 14:58:15 +0100

    build: Install shadcn

--- app/client/src/App.tsx
+++ app/client/src/App.tsx
@@ -7,6 +7,14 @@ function App() {
       <h1 className='text-5xl text-center font-bold'>
         LFC Easy Counter
       </h1>
+
+      <h2>
+        Choose your job
+      </h2>
+
+      <div>
+        
+      </div>
     </>
   )
 }
