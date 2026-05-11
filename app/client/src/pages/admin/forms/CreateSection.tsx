5502d9897ced15338c3c7a8d36bfb7b729c3f0c9
Batubo Victory <victorybatubo76@gmail.com>
2026-05-11 18:35:15 +0100

    feat: Create service and section form pages

---
+++ app/client/src/pages/admin/forms/CreateSection.tsx
@@ -0,0 +1,14 @@
+const CreateSectionPage = () => {
+
+  return (
+    <main className='w-full max-w-md mx-auto px-4 py-10'>
+          <section>
+            <h1>Service Name</h1>
+            <p>Service Date</p>
+            <p>Live counting session</p>
+          </section>
+    </main>
+  )
+}
+
+export default CreateSectionPage
---
+++ app/client/src/pages/admin/forms/CreateService.tsx
@@ -0,0 +1,14 @@
+const CreateServicePage = () => {
+
+  return (
+    <main className='w-full max-w-md mx-auto px-4 py-10'>
+          <section>
+            <h1>Service Name</h1>
+            <p>Service Date</p>
+            <p>Live counting session</p>
+          </section>
+    </main>
+  )
+}
+
+export default CreateServicePage
