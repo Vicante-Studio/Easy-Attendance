b0779210b83385a7be4bcad78c4d1989aecdcb5b
Batubo Victory <victorybatubo76@gmail.com>
2026-05-12 14:23:17 +0100

    feat: Add content on createSection page

--- app/client/src/pages/admin/forms/CreateSection.tsx
+++ app/client/src/pages/admin/forms/CreateSection.tsx
@@ -1,12 +1,20 @@
+import { useNavigate } from 'react-router'
+import { Button } from '@/components/ui/button'
+import SectionForm from '@/components/features/SectionForm'
+
 const CreateSectionPage = () => {
+  const navigate = useNavigate()
 
   return (
-    <main className='w-full max-w-md mx-auto px-4 py-10'>
-          <section>
-            <h1>Service Name</h1>
-            <p>Service Date</p>
-            <p>Live counting session</p>
+    <main className='w-full mx-auto px-4 py-10 flex flex-col gap-8 h-full items-center'>
+          <section className='flex flex-col text-center gap-4 max-w-md'>
+            <h1 className='text-4xl font-bold'>Create a Section</h1>
+            <p>Fill in the form below to create a section</p>
           </section>
+
+          <Button onClick={() => navigate('/adminPage')}>Return to Dashboard</Button>
+
+          <SectionForm />
     </main>
   )
 }
