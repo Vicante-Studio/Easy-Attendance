085257130ae58b48468c2cdc49f9dbd48e53f46f
Batubo Victory <victorybatubo76@gmail.com>
2026-05-16 17:24:42 +0100

    feat: format date so it's readable

--- app/client/src/utils/formatDate.ts
+++ app/client/src/utils/formatDate.ts
@@ -1,12 +1,34 @@
 export const formatDate = (date: string) => {
     const newDate = new Date(date)
 
-    const formattedDate = newDate.toLocaleDateString('en-US', {
-        weekday: 'long',
-        day: 'numeric',
-        month: 'long',
-        year: 'numeric'
+    const getOrdinal = (day: number) => {
+        if (day > 3 && day < 21) return 'th'
+
+            switch (day % 10) {
+            case 1:
+                return 'st'
+            case 2:
+                return 'nd'
+            case 3:
+                return 'rd'
+            default:
+                return 'th'
+            }
+        }
+
+        const day = newDate.getDate()
+
+        const formattedDate = `${newDate.toLocaleDateString('en-US', {
+            weekday: 'long'
+        })} ${day}${getOrdinal(day)} of ${newDate.toLocaleDateString('en-US', {
+            month: 'long'
+        })}`
+
+    const formattedTime = newDate.toLocaleTimeString('en-US', {
+        hour: 'numeric',
+        minute: '2-digit',
+        hour12: true
     })
 
-    return formattedDate
+    return `${formattedDate} at ${formattedTime}`
 }
