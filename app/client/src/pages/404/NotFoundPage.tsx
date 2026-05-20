545aec68bb62d54fa7a2c8f5ba9294b4a3772389
Batubo Victory <victorybatubo76@gmail.com>
2026-05-20 13:52:00 +0100

    feat: Create NotFoundPage

--- app/client/src/pages/404/NotFoundPage.tsx
+++ app/client/src/pages/404/NotFoundPage.tsx
@@ -1,8 +1,13 @@
+import { Button } from '@/components/ui/button'
+import { useNavigate } from 'react-router'
 
 const NotFoundPage = () => {
+  const navigate = useNavigate()
+
   return (
-    <div>
-      
+    <div className='text-center flex flex-col items-center justify-center h-screen border gap-8'>
+      <h1 className='text-5xl font-bold'>Nothing To See Here</h1>
+      <Button className='text-xl px-12 py-6' onClick={() => navigate(-1)}>Go Back</Button>
     </div>
   )
 }
