aaf1ae7e78e0e81a216f11b01a8e8e0b2aea9828
Batubo Victory <victorybatubo76@gmail.com>
2026-05-20 14:22:52 +0100

    feat: Get the active service Id

--- app/client/src/hooks/useActiveService.ts
+++ app/client/src/hooks/useActiveService.ts
@@ -6,12 +6,14 @@ import type { Service } from '@/types/serviceTypes'
 export const useActiveService = () => {
   const [activeService, setActiveService] = useState<Service | null>(null)
   const [loading, setLoading] = useState<boolean>(true)
+  const [activeService_id, setActiveService_id] = useState<string>('')
 
   const fetchActiveService = async () => {
     try {
-      const res = await api.get('/api/churchService/active')
+      const { data } = await api.get('/api/churchService/active')
 
-      setActiveService(res.data)
+      setActiveService(data)
+      setActiveService_id(data.id)
 
     } catch (error) {
       console.error(error)
@@ -34,6 +36,7 @@ export const useActiveService = () => {
 
   return {
     activeService,
-    loading
+    loading,
+    activeService_id
   }
 }
