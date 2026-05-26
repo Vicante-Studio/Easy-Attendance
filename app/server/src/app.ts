316e0d69be581eadd16f303d019a9782766ab063
Batubo Victory <victorybatubo76@gmail.com>
2026-05-26 19:11:31 +0100

    feat: Enable CORS authorization

--- app/server/src/app.ts
+++ app/server/src/app.ts
@@ -11,7 +11,13 @@ import localIPRouter from './routes/serverIP.route.js'
 const app = express()
 const isDev = process.env.NODE_ENV === 'development' || process.env.RUN_SERVER_DIRECTLY === 'true'
 
-app.use(cors({ origin: '*' }))
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    exposedHeaders: ['Content-Disposition'] // IMPORTANT for filename download
+}))
+
 app.use(express.json())
 
 app.use('/api/network', localIPRouter)
